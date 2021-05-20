415086ccd7657ade62759a345f1957ad95268df4
Kenny Ortega <kennyaoc@gmail.com>
2021-05-19 21:57:52 -0400

    Complete queries for nobel table

---
+++ select_nobel.sql
@@ -0,0 +1,8 @@
+-- Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
+-- Show who won the 1962 prize for Literature.
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
+-- Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+-- Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner FROM nobel WHERE yr > 1999 and subject = 'Peace';
