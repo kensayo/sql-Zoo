5fbd152ad279497f029baf96d3f336b2556dbc80
Kenny Ortega <kennyaoc@gmail.com>
2021-05-21 18:07:13 -0400

    Created mored join file

---
+++ more_join.sql
@@ -0,0 +1,25 @@
+-- List the films where the yr is 1962 [Show id, title]
+
+-- Give year of 'Citizen Kane'.
+
+-- List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+
+-- What id number does the actor 'Glenn Close' have?
+
+-- What is the id of the film 'Casablanca'
+
+-- Obtain the cast list for 'Casablanca'.
+
+-- Obtain the cast list for the film 'Alien'
+
+-- List the films in which 'Harrison Ford' has appeared
+
+-- List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+
+-- List the films together with the leading star for all 1962 films.
+
+-- Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+
+-- List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+-- Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
